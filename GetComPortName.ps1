7f025e9e9a4b5294e4b01193fbee9ec1711a6f03
SunmeruRS <38691765+Sunbertrs@users.noreply.github.com>
2024-09-22 15:46:11 +0800

    Improve some function

--- GetComPortName.ps1
+++ GetComPortName.ps1
@@ -1,15 +1,20 @@
 # Get COM port and the bus reported device description script
-# By Sunbertrs
 
 $number = (Get-WMIObject Win32_PnPEntity | where {$_.name -match "com\d"}).Name
-$data = (Get-WMIObject Win32_PnPEntity | where {$_.name -match "com\d"}).GetDeviceProperties("DEVPKEY_Device_BusReportedDeviceDesc").deviceProperties.Data
+try{
+	$name = (Get-WMIObject Win32_PnPEntity | where {$_.name -match "com\d"}).GetDeviceProperties("DEVPKEY_Device_BusReportedDeviceDesc").deviceProperties.Data
+}catch{
+	echo "No COM ports are detected."
+	exit
+}
+
 $i=0
 foreach($num in $number){
 	$num = $num -replace "[^com\d\d]", ""
 	if($exist -eq 1){
 		break
-	}else {
-		[PSCustomObject]@{ Number = $num; Name = $data[$i] }
+	}else{
+		[PSCustomObject]@{ Number = $num; Name = $name[$i] }
 		$exist=1
 	}
 	$i++
